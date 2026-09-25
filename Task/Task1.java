4cc369b05238d71d5de1120d5612f6938fbc2fcc
Pelep <markacebes24@gmail.com>
2026-09-25 11:56:09 +0800

    Change the class into public

--- Task/Task1.java
+++ Task/Task1.java
@@ -2,7 +2,7 @@
  * The HelloWorldApp class implements an application that
  * simply prints "Hello World!" to standard output.
  */
-class Task1 {
+public class Task1 {
     public static void main(String[] args) {
         System.out.println("Kamusta Mundo!"); // Display the string.
     }
