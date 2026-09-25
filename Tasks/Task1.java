ea516294b97b1051c758bdd298174dc41fb8a012
Pelep <markacebes24@gmail.com>
2026-09-25 12:03:24 +0800

    Change the javadoc

--- Tasks/Task1.java
+++ Tasks/Task1.java
@@ -1,6 +1,6 @@
 /**
- * The HelloWorldApp class implements an application that
- * simply prints "Hello World!" to standard output.
+ * The Task1 class implements an application that
+ * simply prints "Kamusta Mundo!" to standard output.
  */
 public class Task1 {
     public static void main(String[] args) {
